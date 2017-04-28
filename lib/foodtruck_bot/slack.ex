558727fee200aca538c3eb3941a25a8cbdb0c412
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2017-04-28 11:57:30 -0400

    Small tweaks
    * Pattern match assignment refactor
    * Don’t pass argument we don’t need or use.

--- lib/foodtruck_bot/slack.ex
+++ lib/foodtruck_bot/slack.ex
@@ -1,16 +1,16 @@
 defmodule FoodtruckBot.Slack do
   use Slack
 
-  def handle_event(message = %{type: "message"}, slack, state) do
+  def handle_event(%{type: "message"} = message, slack, state) do
     if Map.has_key?(message, :text) do
-      parse_message(message, slack, state)
+      parse_message(message, slack)
     end
 
     {:ok, state}
   end
   def handle_event(_, _, state), do: {:ok, state}
 
-  defp parse_message(message, slack, state) do
+  defp parse_message(message, slack) do
     if Regex.run ~r/<@#{slack.me.id}>:?\s+.*today.*/, message.text do
       Task.start fn() -> FoodtruckBot.Twitter.fetch_trucks(message, slack) end
       send_message("Checking today's trucks...", message.channel, slack) 
