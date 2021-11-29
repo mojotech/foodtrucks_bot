e4a9e1514843f0d7a9bccf6eef5593a4de3946cc
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2021-11-29 11:25:22 -0500

    [mix] Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FoodtruckBot.Mixfile do
   def project do
     [
       app: :foodtruck_bot,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
